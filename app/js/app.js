b74df96440cb0541f3a734968fe03bc120869856
Casey Thomas <cpthomas@gmail.com>
2013-02-09 11:52:12 -0500

    Format money

--- app/js/app.js
+++ app/js/app.js
@@ -38,8 +38,11 @@ app.getDimValue = function(id, data) {
   return dimValue;
 };
 
-app.formatMoney = function(dimValue) {
-  
+//http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+Number.prototype.formatMoney = function(){
+  var c=2, d='.', t=',';
+  var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 
 d3.json("data/summary.json", function(error, data){
@@ -72,7 +75,7 @@ d3.json("data/summary.json", function(error, data){
         d3.select('#neighborhood span').html(d.id);
 
         var dimValue = app.getDimValue(d.id, data) || 0;
-        d3.select('#dim-value span').html(dimValue);
+        d3.select('#dim-value span').html('$' + dimValue.formatMoney());
       })
       .on("click", function(d) {
 
