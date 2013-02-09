3788c1225687761014fbfa5a06fdd4ac0076cda0
Casey Thomas <cpthomas@gmail.com>
2013-02-09 14:06:41 -0500

    Show property information when selection is made from search menu

--- server/server.js
+++ server/server.js
@@ -11,25 +11,12 @@ var nowjs = require('now'),
     models = require('./models'),
     Property = models.Property;
 
-    var prop1 = new Property({
-      address: '1000 Market St Foo',
-      dimValue: -30,
-      count: 333
-    });
-    prop1.save();
-
 // now js search function
 everyone.now.search = function(text, count, callback) {
   // create regex for "contains" and ignore case
   var regex = new RegExp(text.term, 'i');
   // execute the search
   Property.find({address: regex}, function(err, docs) {
-    var props = [];
-    for(var prop in docs) {
-      // push the firstname to the array
-      props.push(docs[prop].address);
-    }
-    // send back via callback function
-    callback(null, props);
+    callback(null, docs);
   });
 };
