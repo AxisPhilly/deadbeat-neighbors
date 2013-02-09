8e1fb718e9856e61b503a04afc0603a22807e40d
Casey Thomas <cpthomas@gmail.com>
2013-02-09 10:25:41 -0500

    Add script to total diminished value by 'hood

---
+++ scripts/create-summary.js
@@ -0,0 +1,43 @@
+var csv = require('csv');
+var fs = require('fs');
+
+var output = [],
+    total = 0;
+
+csv()
+  .from(__dirname + '/data/props_w_dim_value_hoods.csv', {columns: true})
+  .on('record', function(data, index) {
+    var found = 0;
+
+    output.forEach(function(item){
+      if(data.hood === item.hood) {
+        found = 1;
+
+        item.dimValue += Number(data.dim_value);
+      }
+    });
+
+    if (!found) {
+      output.push({
+        hood: data.hood,
+        dimValue: Number(data.dim_value)
+      });
+    }
+
+    total += Number(data.dim_value);
+  })
+  .on('end', function(count) {
+    console.log('Number of lines: ' + count);
+
+    fs.writeFile(__dirname + '/data/summary.json', JSON.stringify(output, null, 2), function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log(total);
+        console.log('JSON saved to ' + __dirname + '/data/summary.json');
+      }
+    });
+  })
+  .on('error', function(error) {
+    console.log(error.message);
+  });
